c129b99dcdb3a965e241d6527f3a17314ba27f16
dominikkarall <dominik.karall@gmail.com>
2020-07-17 20:25:09 +0200

    add helloworld example add TODO

---
+++ FHEM/bindings/python/lib/helloworld/helloworld.py
@@ -0,0 +1,38 @@
+
+import asyncio
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+from .. import fhem
+
+class helloworld:
+
+    def __init__(self):
+        return
+
+    # FHEM FUNCTION
+    async def Define(self, hash, args, argsh):
+        await fhem.readingsBeginUpdate(hash)
+        await fhem.readingsBulkUpdateIfChanged(hash, "state", "on")
+        await fhem.readingsBulkUpdateIfChanged(hash, "hello", "world")
+        await fhem.readingsEndUpdate(hash, 1)
+
+        return ""
+
+    # FHEM FUNCTION
+    async def Undefine(self, hash, args, argsh):
+        return
+
+    # FHEM FUNCTION
+    async def Set(self, hash, args, argsh):
+        if (len(args) < 2 or args[1] == "?"):
+            return ("Unknown argument ?, choose one of "
+                    "on:noArg off:noArg")
+        else:
+            action = args[1]
+            if (action == "on"):
+                await fhem.readingsSingleUpdate(hash, "state", "on", 1)
+            else:
+                await fhem.readingsSingleUpdate(hash, "state", "off", 1)
---
+++ TODO.md
@@ -0,0 +1,10 @@
+# TODOs
+
+## Generic
+ - use verbose from FHEM module to define logger DEBUG/INFO/...
+ - create sample with threading to allow usage of blocking functions
+
+## Module specific
+### mdnsscanner
+ - check if device exists already before CommandDefine
+
