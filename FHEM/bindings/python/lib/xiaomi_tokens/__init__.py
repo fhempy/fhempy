4386a399fa9a45bd4e80465cf504f263ddedfa3d
dominikkarall <dominik.karall@gmail.com>
2020-10-11 12:19:16 +0200

    add xiaomi token retriever

---
+++ FHEM/bindings/python/lib/xiaomi_tokens/__init__.py
@@ -0,0 +1,1 @@
+# Xiaomi Token retriever
---
+++ FHEM/bindings/python/lib/xiaomi_tokens/xiaomi_tokens.py
@@ -0,0 +1,68 @@
+
+import asyncio
+import functools
+
+from micloud import MiCloud
+
+from .. import utils
+from .. import fhem
+
+class xiaomi_tokens:
+
+    def __init__(self, logger):
+        self.logger = logger
+        self._username = None
+        self._password = None
+        self._country = "de"
+        return
+
+    # FHEM FUNCTION
+    async def Define(self, hash, args, argsh):
+        self.hash = hash
+        return ""
+
+    # FHEM FUNCTION
+    async def Undefine(self, hash):
+        return
+
+    # FHEM FUNCTION
+    async def Set(self, hash, args, argsh):
+        set_list_conf = {
+           "username": { "args": ["username"] },
+           "password": { "args": ["password"] },
+           "country": { "args": ["country"], "format": "de,cn,sg" },
+           "get_tokens": {}
+        }
+        return utils.handle_set(set_list_conf, self, hash, args, argsh)
+
+    async def set_username(self, hash, params):
+        self._username = params['username']
+        return ""
+
+    async def set_password(self, hash, params):
+        self._password = params['password']
+        return ""
+
+    async def set_country(self, hash, params):
+        self._country = params['country']
+        return ""
+
+    async def set_get_tokens(self, hash):
+        if self._username and self._password:
+            asyncio.create_task(self.obtain_tokens())
+        else:
+            return "Please set username & password first!"
+
+    async def obtain_tokens(self):
+        await utils.run_blocking(functools.partial(self.thread_get_tokens))
+        await fhem.readingsBeginUpdate(self.hash)
+        for dev in self._device_list:
+            await fhem.readingsBulkUpdateIfChanged(self.hash, dev['did'] + "_name", dev['name'])
+            await fhem.readingsBulkUpdateIfChanged(self.hash, dev['did'] + "_token", dev['token'])
+            await fhem.readingsBulkUpdateIfChanged(self.hash, dev['did'] + "_model", dev['model'])
+        await fhem.readingsEndUpdate(self.hash, 1)
+    
+    def thread_get_tokens(self):
+        mc = MiCloud(self._username, self._password)
+        mc.login()
+        self._device_list = mc.get_devices(country=self._country)
