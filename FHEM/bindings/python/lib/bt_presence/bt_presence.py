812c3e23ddf4a12fa21b6042220cedddf8295f9e
dominikkarall <dominik.karall@gmail.com>
2020-10-20 20:42:55 +0200

    support some attributes

--- FHEM/bindings/python/lib/bt_presence/bt_presence.py
+++ FHEM/bindings/python/lib/bt_presence/bt_presence.py
@@ -14,12 +14,23 @@ class bt_presence:
         self.logger = logger
         self.hash = None
         self.btscan_task = None
-        return
+        self._interval = {
+            "absent": 10,
+            "present": 60
+        }
+        self._threshold = {
+            "absent": 0,
+            "present": 0
+        }
+        self._curr_state = ""
+        self._count_diff_state = 0
 
     def lookup_name(self, mac):
         return bluetooth.lookup_name(mac, timeout=5)
 
     async def run_bt_scan(self):
+        curr_name = ""
+        curr_rssi = 0
         while True:
             new_state = "absent"
             try:
@@ -30,19 +41,28 @@ class bt_presence:
                         break
 
                 if device_name:
-                    await fhem.readingsSingleUpdateIfChanged(self.hash, "name", device_name, 1)
                     self._btrssi = BluetoothRSSI(self._address)
                     rssi = await utils.run_blocking(functools.partial(self._btrssi.request_rssi))
                     self._btrssi.close()
-                    await fhem.readingsSingleUpdateIfChanged(self.hash, "rssi", rssi[0], 1)
+                    rssi = rssi[0]
+                    if curr_name != device_name:
+                        curr_name = device_name
+                        await fhem.readingsSingleUpdateIfChanged(self.hash, "name", device_name, 1)
+                    if curr_rssi != rssi:
+                        curr_rssi = rssi
+                        await fhem.readingsSingleUpdateIfChanged(self.hash, "rssi", rssi, 1)
                     new_state = "present"
             except:
                 self.logger.exception("BleakScanner failed")
-            await self.update_state(new_state)
-            if new_state == "absent":
-                await asyncio.sleep(10)
-            else:
-                await asyncio.sleep(60)
+            
+            if self._curr_state != new_state:
+                self._count_diff_state += 1
+                if self._count_diff_state >= self._threshold[new_state]:
+                    self._curr_state = new_state
+                    self._count_diff_state = 0
+                    await self.update_state(self._curr_state)
+
+            await asyncio.sleep(self._interval[self._curr_state])
 
     async def update_state(self, new_state):
         await fhem.readingsSingleUpdateIfChanged(self.hash, "presence", new_state, 1)
@@ -56,7 +76,17 @@ class bt_presence:
 
         self._address = args[3]
         self.hash["MAC"] = args[3]
-        await self.update_state("absent")
+
+        await fhem.addToDevAttrList(hash["NAME"], "absentInterval presentInterval absentThreshold presentThreshold")
+
+        self._interval = {
+            "absent": int(await fhem.AttrVal(hash["NAME"], "absentInterval", "60")),
+            "present": int(await fhem.AttrVal(hash["NAME"], "presentInterval", "60"))
+        }
+        self._threshold = {
+            "absent": int(await fhem.AttrVal(hash["NAME"], "absentThreshold", "0")),
+            "present": int(await fhem.AttrVal(hash["NAME"], "presentThreshold", "0"))
+        }
 
         if self.btscan_task:
             self.btscan_task.cancel()
@@ -67,3 +97,28 @@ class bt_presence:
     async def Undefine(self, hash, args, argsh):
         if self.btscan_task:
             self.btscan_task.cancel()
+
+    async def Attr(self, hash, args, argsh):
+        cmd = args[0]
+        name = args[1]
+        attr_name = args[2]
+        attr_val = args[3]
+
+        if cmd == "set":
+            if attr_name == "absentInterval":
+                self._interval['absent'] = int(attr_val)
+            elif attr_name == "presentInterval":
+                self._interval['present'] = int(attr_val)
+            elif attr_name == "absentThreshold":
+                self._threshold['absent'] = int(attr_val)
+            elif attr_name == "presentThreshold":
+                self._threshold['present'] = int(attr_val)
+        else:
+            if attr_name == "absentInterval":
+                self._interval['absent'] = 10
+            elif attr_name == "presentInterval":
+                self._interval['present'] = 60
+            elif attr_name == "absentThreshold":
+                self._threshold['absent'] = 0
+            elif attr_name == "presentThreshold":
+                self._threshold['present'] = 0
