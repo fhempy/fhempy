9daa50added2dfb302958d026ec7b93e1a05c8e8
dominikkarall <dominik.karall@gmail.com>
2020-11-07 21:21:42 +0100

    add attr interval, hci_device

--- FHEM/bindings/python/lib/miflora/miflora.py
+++ FHEM/bindings/python/lib/miflora/miflora.py
@@ -18,7 +18,12 @@ class miflora:
         self.hash = None
         self.updateTask = None
         self._poller = None
-        self._update_interval = 1200
+        self._attr_update_interval = 1200
+        self._attr_hci_device = "hci0"
+        self._attr_list = {
+            "update_interval": {"default": 1200, "format": "int"},
+            "hci_device": {"default": "hci0"}
+        }
         return
 
     # FHEM FUNCTION
@@ -27,13 +32,14 @@ class miflora:
         if len(args) < 4:
             return "Usage: define mi_plant PythonModule miflora <MAC>"
 
+        await utils.handle_define_attr(self._attr_list, self, hash)
         self._address = args[3]
         self.hash["MAC"] = args[3]
 
         self._poller = miflora_poller.MiFloraPoller(
                 self._address,
-                cache_timeout=self._update_interval,
-                adapter="hci0",
+                cache_timeout=60,
+                adapter=self._attr_hci_device,
                 backend=btlewrap.BluepyBackend,
             )
 
@@ -56,7 +62,19 @@ class miflora:
                 param_val = await utils.run_blocking(self._poller.parameter_value(param))
                 await fhem.readingsBulkUpdateIfChanged(self.hash, param, param_val)
             await fhem.readingsEndUpdate(self.hash, 1)
-            await asyncio.sleep(self._update_interval)
+            await asyncio.sleep(self._attr_update_interval)
+
+    # FHEM FUNCTION
+    async def Attr(self, hash, args, argsh):
+        return await utils.handle_attr(self._attr_list, self, hash, args, argsh)
+
+    async def set_attr_hci_device(self, hash):
+        self._poller = miflora_poller.MiFloraPoller(
+                self._address,
+                cache_timeout=60,
+                adapter=self._attr_hci_device,
+                backend=btlewrap.BluepyBackend,
+            )
 
     # FHEM FUNCTION
     async def Undefine(self, hash):
