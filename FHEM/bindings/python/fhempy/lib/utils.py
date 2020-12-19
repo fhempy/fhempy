2c7ff8a9bf2b0b00ea5cfc89db318e708e6f9434
dominikkarall <dominik.karall@gmail.com>
2020-12-19 09:10:14 +0100

    fix "too many params"

--- FHEM/bindings/python/fhempy/lib/utils.py
+++ FHEM/bindings/python/fhempy/lib/utils.py
@@ -165,7 +165,7 @@ async def handle_set(set_list_conf, obj, hash, args, argsh):
             for arg in args[2:]:
                 # arg ... mode
                 # all_args[mode] = mode argument
-                if "args" in cmd_def and i in cmd_def["args"]:
+                if "args" in cmd_def and i < len(cmd_def["args"]):
                     all_args[cmd_def["args"][i]] = arg
                 else:
                     return f"Too many parameters provided: {arg}"
