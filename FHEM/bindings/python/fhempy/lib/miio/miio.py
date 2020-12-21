d011605785dac49633a6260fa19890b1400a4b7c
dominikkarall <dominik.karall@gmail.com>
2020-12-21 20:57:07 +0100

    fix possible deadlock

--- FHEM/bindings/python/fhempy/lib/miio/miio.py
+++ FHEM/bindings/python/fhempy/lib/miio/miio.py
@@ -20,7 +20,7 @@ class miio(FhemModule):
         self._attr_list = {
             "update_functions": {
                 "default": "status:60,info:600",
-                "help": "Define command which should be executed every X seconds.<br>info:600 executes info every 600s",
+                "help": "Define command which should be executed every X seconds.<br>info:600 executes info every 600s<br>Default: status:60,info:600",
             }
         }
         self.set_attr_config(self._attr_list)
@@ -83,7 +83,7 @@ class miio(FhemModule):
             for fct_upd in fct_upd_list:
                 sec = int(fct_upd.split(":")[1])
                 fct = fct_upd.split(":")[0]
-                self._fct_update_tasks[fct] = asyncio.create_task(
+                self._fct_update_tasks[fct] = self.create_async_task(
                     self.fct_update_loop(fct, sec)
                 )
 
@@ -91,21 +91,19 @@ class miio(FhemModule):
         while True:
             try:
                 await self.send_command(fct_name, None)
+                await fhem.readingsSingleUpdateIfChanged(
+                    self.hash, "presence", "online", 1
+                )
             except:
-                self.logger.exception(f"Failed to send_command: {fct_name}")
+                self.logger.error(f"Failed to send_command: {fct_name}")
+                await fhem.readingsSingleUpdateIfChanged(
+                    self.hash, "presence", "offline", 1
+                )
             await asyncio.sleep(sec)
-
-    # FHEM FUNCTION
-    async def Undefine(self, hash):
-        # cancel fct update tasks
-        for task in self._fct_update_tasks.copy():
-            task.cancel()
-            del self._fct_update_tasks[task]
-        return
 
     async def set_command(self, hash, params):
         cmd = params["cmd"]
-        asyncio.create_task(self.send_command(cmd, params))
+        self.create_async_task(self.send_command(cmd, params))
 
     def is_number(self, string):
         try:
@@ -157,7 +155,7 @@ class miio(FhemModule):
                             self.hash, prop, st[prop]
                         )
             except:
-                await fhem.readingsBulkUpdateIfChanged(self.hash, fct.__name__, reply)
+                pass
             await fhem.readingsEndUpdate(self.hash, 1)
         else:
             if (
