bc2dcc9b5b623ad132ed16e75315be9b21172ed8
dominikkarall <dominik.karall@gmail.com>
2020-12-22 21:59:39 +0100

    support create device out of xiaomi_token

--- FHEM/bindings/python/fhempy/lib/xiaomi_tokens/xiaomi_tokens.py
+++ FHEM/bindings/python/fhempy/lib/xiaomi_tokens/xiaomi_tokens.py
@@ -14,13 +14,14 @@ class xiaomi_tokens(FhemModule):
         self._username = None
         self._password = None
         self._country = ["de", "cn", "sg"]
+        self._all_devices = {}
 
-        set_list_conf = {
+        self._set_list_conf = {
             "username": {"args": ["username"]},
             "password": {"args": ["password"]},
             "get_tokens": {},
         }
-        self.set_set_config(set_list_conf)
+        self.set_set_config(self._set_list_conf)
         return
 
     # FHEM FUNCTION
@@ -40,6 +41,10 @@ class xiaomi_tokens(FhemModule):
         )
         if self._enc_password != "":
             self._password = utils.decrypt_string(self._enc_password, self._uniqueid)
+
+        # retrieve tokens
+        if self._username and self._password:
+            self.create_async_task(self.obtain_tokens())
         return ""
 
     async def set_username(self, hash, params):
@@ -64,6 +69,54 @@ class xiaomi_tokens(FhemModule):
         else:
             return "Please set username & password first!"
 
+    async def set_create_miio_device(self, hash, params):
+        blank = params["dev"].index("_")
+        did = params["dev"][0:blank]
+        model = self._all_devices[did]["model"]
+        ip = self._all_devices[did]["localip"]
+        token = self._all_devices[did]["token"]
+        if "vacuum" in model:
+            self.create_async_task(
+                fhem.CommandDefine(
+                    self.hash,
+                    f"miio_vacuum_{did} PythonModule miio vacuum {ip} {token}",
+                )
+            )
+        elif "viomi" in model:
+            self.create_async_task(
+                fhem.CommandDefine(
+                    self.hash,
+                    f"miio_vacuum_{did} PythonModule miio viomivacuum {ip} {token}",
+                )
+            )
+        elif "chuangmi" in model or "camera" in model:
+            self.create_async_task(
+                fhem.CommandDefine(
+                    self.hash,
+                    f"miio_camera_{did} PythonModule miio chuangmicamera {ip} {token}",
+                )
+            )
+        else:
+            self.create_async_task(
+                fhem.CommandDefine(
+                    self.hash,
+                    f"miio_device_{did} PythonModule miio device {ip} {token}",
+                )
+            )
+
+    async def set_create_gateway3_device(self, hash, params):
+        blank = params["dev"].index("_")
+        did = params["dev"][0:blank]
+        ip = self._all_devices[did]["localip"]
+        token = self._all_devices[did]["token"]
+        if ip != "" and token != "":
+            self.create_async_task(
+                fhem.CommandDefine(
+                    self.hash,
+                    f"xiaomigw3_{did} PythonModule xiaomi_gateway3 {ip} {token}",
+                )
+            )
+
     async def obtain_tokens(self):
         try:
             await utils.run_blocking(functools.partial(self.thread_get_tokens))
@@ -71,6 +124,8 @@ class xiaomi_tokens(FhemModule):
             await fhem.readingsSingleUpdateIfChanged(f"Failed to get tokens: {e}")
             return
 
+        self._miio_devices = []
+        self._xiaomigw3_devices = []
         await fhem.readingsBeginUpdate(self.hash)
         for dev in self._device_list:
             await fhem.readingsBulkUpdateIfChanged(
@@ -85,7 +140,28 @@ class xiaomi_tokens(FhemModule):
             await fhem.readingsBulkUpdateIfChanged(
                 self.hash, dev["did"] + "_ip", dev["localip"]
             )
+            self._all_devices[dev["did"]] = dev
+            if dev["did"][0:3] != "blt" and dev["localip"] != "":
+                self._miio_devices.append(
+                    dev["did"] + "_(" + dev["name"].replace(" ", "_") + ")"
+                )
+                if dev["model"] == "lumi.gateway.mgl03":
+                    self._xiaomigw3_devices.append(
+                        dev["did"] + "_(" + dev["name"].replace(" ", "_") + ")"
+                    )
         await fhem.readingsEndUpdate(self.hash, 1)
+
+        self._set_list_conf["create_miio_device"] = {
+            "args": ["dev"],
+            "help": "NOT YET SUPPORTED<br>Creates a fhempy miio device to control your Xiaomi device.",
+            "options": ",".join(self._miio_devices),
+        }
+        self._set_list_conf["create_gateway3_device"] = {
+            "args": ["dev"],
+            "help": "Creates a fhempy xiaomi_gateway3 device and devices for all connected devices.",
+            "options": ",".join(self._xiaomigw3_devices),
+        }
+        self.set_set_config(self._set_list_conf)
 
     def thread_get_tokens(self):
         self._device_list = []
