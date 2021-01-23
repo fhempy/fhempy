5209d32cf97c2aea43ee4c042c914b9a8474371c
dominikkarall <dominik.karall@gmail.com>
2021-01-23 22:15:23 +0100

    keep asyncio debug slow coroutine 0.1s

--- FHEM/bindings/python/fhempy/lib/fhem_pythonbinding.py
+++ FHEM/bindings/python/fhempy/lib/fhem_pythonbinding.py
@@ -460,7 +460,6 @@ def run():
 
     logger.info("Waiting for FHEM connection")
     asyncio.get_event_loop().set_debug(True)
-    asyncio.get_event_loop().slow_callback_duration = 0.05
     asyncio.get_event_loop().run_until_complete(
         websockets.serve(
             pybinding, "0.0.0.0", port, ping_timeout=None, ping_interval=None
