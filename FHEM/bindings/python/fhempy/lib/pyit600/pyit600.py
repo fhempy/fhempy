cfacd627e3fbcbcbf52e0254c033a8f14193a156
staeblvo <v.staeblein@gmx.de>
2022-01-28 15:09:28 +0100

    additional attributes added

--- FHEM/bindings/python/fhempy/lib/pyit600/pyit600.py
+++ FHEM/bindings/python/fhempy/lib/pyit600/pyit600.py
@@ -23,7 +23,22 @@ class pyit600(generic.FhemModule):
             "update_readings": {
                 "default": "always",
                 "options": "always,onchange",
-                "help": "Update readings only on value change or always (default onchange).",
+                "help": "Update readings only on value change or always (default always).",
+            },
+            "update_min_interval": {
+                "default": 0,
+                "format": "int",
+                "help": "Update by interval in combination with onchange. Add multiplier (default 0 -> off, e.g. 5 -> 5x update_interval).",
+            },
+            "readings_device_id": {
+                "default": "off",
+                "options": "off,on",
+                "help": "Create readings with device ID, also if device name is available (default off).",
+            },
+            "readings_cmpl_dev_str": {
+                "default": "on",
+                "options": "on,off",
+                "help": "Create readings with all values of device (default on).",
             },
         }
         self.set_attr_config(self.attr_config)
@@ -38,6 +53,7 @@ class pyit600(generic.FhemModule):
         self.host = args[3]
         self.euid = args[4]
         self.debug = 1
+        self.min_interval_timer = 1 
         _LOGGER.debug(f'host: %s, euid: %s',self.host, self.euid)
         self.create_async_task(self.start_login())
         await fhem.readingsSingleUpdate(self.hash, "state", "connecting", 1)
@@ -66,7 +82,7 @@ class pyit600(generic.FhemModule):
                     "temperature": {"default": 23, "format": "float"}},
                 "help": (
                     "Parameters: deviceID Temperature <br>"
-                    "e.g. set target_temperature 001e5e09020770e3 23<br>"
+                    "e.g. set my_salus_device target_temperature 001e5e09020770e3 23<br>"
                 )
             }
         }
@@ -105,16 +121,24 @@ class pyit600(generic.FhemModule):
             else:
                 await fhem.readingsSingleUpdateIfChanged(self.hash, "state", "online", 1)
 
-                for climate_device_id in climate_devices:
-                    if self._attr_update_readings == "always":
-                        await fhem.readingsSingleUpdate(
-                            self.hash, climate_device_id, climate_devices.get(climate_device_id), 1)
+                if (self._attr_update_readings == "always") or \
+                   ((self._attr_update_min_interval > 0) and (self.min_interval_timer >= self._attr_update_min_interval)):
+                   
+                    self.min_interval_timer = 1
+                    
+                    for climate_device_id in climate_devices:
+                        
+                        if self._attr_readings_cmpl_dev_str == "on":
+                            await fhem.readingsSingleUpdate(
+                                self.hash, climate_device_id, climate_devices.get(climate_device_id), 1)
+                                
                         try:
                             name = re.search('name=''(.+?)'',', str(climate_devices.get(climate_device_id))).group(1)
                             name = name.replace(" ", "")
                             name = name.replace("'", "")
                             curtemp = re.search('current_temperature=(.+?),', str(climate_devices.get(climate_device_id))).group(1)
-                            if name == 'unknown':
+                            
+                            if name == 'unknown' or self._attr_readings_device_id == "on":
                                 await fhem.readingsSingleUpdate(self.hash, climate_device_id + "_curtemp", curtemp, 1)
                             else:
                                 await fhem.readingsSingleUpdate(self.hash, name + "_curtemp", curtemp, 1)
@@ -126,22 +150,29 @@ class pyit600(generic.FhemModule):
                             name = name.replace(" ", "")
                             name = name.replace("'", "")
                             targtemp = re.search('target_temperature=(.+?),', str(climate_devices.get(climate_device_id))).group(1)
-                            if name == 'unknown':
+                            
+                            if name == 'unknown' or self._attr_readings_device_id == "on":
                                 await fhem.readingsSingleUpdate(self.hash, climate_device_id + "_targtemp", targtemp, 1)
                             else:
                                 await fhem.readingsSingleUpdate(self.hash, name + "_targtemp", targtemp, 1)
                         except AttributeError:
                             _LOGGER.info(f'Error on reading device name or target temperature!')
 
-                    else:
-                        await fhem.readingsSingleUpdateIfChanged(
-                            self.hash, climate_device_id, climate_devices.get(climate_device_id), 1)
+                else:
+                    self.min_interval_timer += 1
+
+                    for climate_device_id in climate_devices:
+                        if self._attr_readings_cmpl_dev_str == "on":
+                            await fhem.readingsSingleUpdateIfChanged(
+                                self.hash, climate_device_id, climate_devices.get(climate_device_id), 1)
+                                
                         try:
                             name = re.search('name=''(.+?)'',', str(climate_devices.get(climate_device_id))).group(1)
                             name = name.replace(" ", "")
                             name = name.replace("'", "")
                             curtemp = re.search('current_temperature=(.+?),', str(climate_devices.get(climate_device_id))).group(1)
-                            if name == 'unknown':
+                            
+                            if name == 'unknown' or self._attr_readings_device_id == "on":
                                 await fhem.readingsSingleUpdateIfChanged(self.hash, climate_device_id + "_curtemp", curtemp, 1)
                             else:
                                 await fhem.readingsSingleUpdateIfChanged(self.hash, name + "_curtemp", curtemp, 1)
@@ -153,7 +184,8 @@ class pyit600(generic.FhemModule):
                             name = name.replace(" ", "")
                             name = name.replace("'", "")
                             targtemp = re.search('target_temperature=(.+?),', str(climate_devices.get(climate_device_id))).group(1)
-                            if name == 'unknown':
+                            
+                            if name == 'unknown' or self._attr_readings_device_id == "on":
                                 await fhem.readingsSingleUpdateIfChanged(self.hash, climate_device_id + "_targtemp", targtemp, 1)
                             else:
                                 await fhem.readingsSingleUpdateIfChanged(self.hash, name + "_targtemp", targtemp, 1)
