d36bea4249350ea493757406328602f1b8e7a400
dominikkarall <dominik.karall@gmail.com>
2022-02-18 21:47:41 +0100

    fix(zigbee2mqtt): fix restart

--- FHEM/bindings/python/fhempy/lib/zigbee2mqtt/zigbee2mqtt.py
+++ FHEM/bindings/python/fhempy/lib/zigbee2mqtt/zigbee2mqtt.py
@@ -21,6 +21,7 @@ class zigbee2mqtt(FhemModule):
             "disable": {"default": "0", "options": "0,1", "format": "int"}
         }
         self.set_attr_config(self._attr_list)
+        self.check_process_task = None
 
     # FHEM FUNCTION
     async def Define(self, hash, args, argsh):
@@ -32,15 +33,15 @@ class zigbee2mqtt(FhemModule):
         self.create_async_task(self.run_z2m())
 
     async def run_z2m(self):
-        res = await self.install_z2m()
-        if res is False:
-            return
-
         res = await self.check_node_installation()
         if res is False:
             return
 
         res = await self.check_npm_installation()
+        if res is False:
+            return
+
+        res = await self.install_z2m()
         if res is False:
             return
 
@@ -210,7 +211,7 @@ class zigbee2mqtt(FhemModule):
         try:
             self.proc = subprocess.Popen(["npm", "start"], cwd=z2m_directory)
             await fhem.readingsSingleUpdate(self.hash, "state", "running", 1)
-            self.create_async_task(self.check_process())
+            self.check_process_task = self.create_async_task(self.check_process())
         except Exception:
             self.logger.exception("Failed to start zigbee2mqtt with npm start")
 
@@ -227,12 +228,16 @@ class zigbee2mqtt(FhemModule):
                         self.hash, "state", "error, check log file", 1
                     )
                     return
+                elif poll == 0:
+                    await self.start_process()
             await asyncio.sleep(10)
 
     async def stop_process(self):
         if self.proc:
             self.proc.terminate()
             self.proc = None
+        if self.check_process_task:
+            self.cancel_async_task(self.check_process_task)
         await fhem.readingsSingleUpdate(self.hash, "state", "stopped", 1)
 
     async def create_weblink(self):
