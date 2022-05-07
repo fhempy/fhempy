3c73b8312ae71d9fa74e23d7d9299e40ece61951
dominikkarall <dominik.karall@gmail.com>
2022-05-07 13:39:54 +0200

    feat(fusionsolar): support from/to_grid, electrical_load, grid_power and inverter_output_power

--- FHEM/bindings/python/fhempy/lib/fusionsolar/fusionsolar.py
+++ FHEM/bindings/python/fhempy/lib/fusionsolar/fusionsolar.py
@@ -3,7 +3,10 @@ import functools
 import re
 from urllib.parse import urlparse
 
-from fhempy.lib.fusionsolar.fusionsolar_api import FusionSolarKioksApi
+from fhempy.lib.fusionsolar.fusionsolar_api import (
+    FusionSolarKioksApi,
+    FusionSolarRestApi,
+)
 
 from .. import fhem, utils
 from .. import generic
@@ -12,19 +15,32 @@ from .. import generic
 class fusionsolar(generic.FhemModule):
     def __init__(self, logger):
         super().__init__(logger)
+        self._stationid = None
+        self._sessionid = None
 
     # FHEM FUNCTION
     async def Define(self, hash, args, argsh):
         await super().Define(hash, args, argsh)
-        if len(args) != 3 or len(argsh) != 1:
-            return "Usage: define my_solar fhempy fusionsolar KIOSKURL"
+        if not (len(args) == 3 or len(args) == 4 or len(args) == 5) or len(argsh) != 2:
+            return (
+                "Usage: define my_solar fhempy fusionsolar "
+                "KIOSKURL [STATIONID] [SESSIONID] [REGION]"
+            )
         self._url = list(argsh)[0] + "=" + argsh[list(argsh)[0]]
+        if len(args) >= 4:
+            self._stationid = list(argsh)[1] + "=" + argsh[list(argsh)[1]]
+            self._sessionid = args[3]
+            self._region = args[4] if len(args) > 4 else "region01eu05"
+            self.restapi = FusionSolarRestApi(
+                self.logger, self._stationid, self._sessionid, self._region
+            )
+
         await fhem.readingsSingleUpdate(hash, "state", "connecting", 1)
         self.create_async_task(self.update())
 
     async def update(self):
         self.kiosk = Kiosk(self._url)
-        self.api = FusionSolarKioksApi(self.kiosk.apiUrl())
+        self.kioskapi = FusionSolarKioksApi(self.kiosk.apiUrl())
 
         await self.update_readings()
 
@@ -33,8 +49,28 @@ class fusionsolar(generic.FhemModule):
             await fhem.readingsBeginUpdate(self.hash)
             try:
                 data = await utils.run_blocking(
-                    functools.partial(self.api.getRealTimeKpi, self.kiosk.id)
+                    functools.partial(self.kioskapi.getRealTimeKpi, self.kiosk.id)
                 )
+                if self._sessionid:
+                    await self.restapi.update()
+
+                    await fhem.readingsBulkUpdate(
+                        self.hash, "from_grid", self.restapi.from_grid
+                    )
+                    await fhem.readingsBulkUpdate(
+                        self.hash, "to_grid", self.restapi.to_grid
+                    )
+                    await fhem.readingsBulkUpdate(
+                        self.hash, "electrical_load", self.restapi.electrical_load
+                    )
+                    await fhem.readingsBulkUpdate(
+                        self.hash, "grid_power", self.restapi.grid_power
+                    )
+                    await fhem.readingsBulkUpdate(
+                        self.hash,
+                        "inverter_output_power",
+                        self.restapi.inverter_output_power,
+                    )
                 await fhem.readingsBulkUpdate(
                     self.hash, "realtime_power", data["realTimePower"]
                 )
