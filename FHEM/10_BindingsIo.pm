5c98ef5c987c5076debf5324bd26f9ba4bb1e310
dominikkarall <dominik.karall@gmail.com>
2022-08-12 22:34:20 +0200

    fix(fhempy): fix continue in some cases and fix devStateIcon

--- FHEM/10_BindingsIo.pm
+++ FHEM/10_BindingsIo.pm
@@ -63,7 +63,7 @@ BindingsIo_Define($$$)
 
   my $port = 0;
   my $localServer = 1;
-  if ($bindingType eq "Python" || $bindingType eq "fhempy") {
+  if ($bindingType eq "Python" or $bindingType eq "fhempy") {
     $hash->{DeviceName} = "ws:127.0.0.1:15733";
     $hash->{IP} = "127.0.0.1";
     $hash->{PORT} = "15733";
@@ -113,7 +113,7 @@ BindingsIo_Define($$$)
   }
   # set devStateIcon
   my $devstateicon_val = AttrVal($name, "devStateIcon", "");
-  if ($devstateicon_val eq "" || index($devstateicon_val, "ver_available") == -1) {
+  if ($devstateicon_val eq "" or index($devstateicon_val, "ver_available") == -1) {
     my $devstate_cmd = '{
       my $status_img = "10px-kreis-gruen";;
       my $status_txt = "connected";;
@@ -347,6 +347,8 @@ BindingsIo_Write($$$$$) {
     if ($returnval ne "empty" && $returnval ne "continue") {
       $timeouts = 0;
       last;
+    } else {
+      $returnval = "";
     }
   }
   Log3 $hash, 4, "BindingsIo ($hash->{NAME}): end ".$hash->{BindingType}."Function: ".$devhash->{NAME}." => $function ($waitingForId) - result: ".$returnval;
