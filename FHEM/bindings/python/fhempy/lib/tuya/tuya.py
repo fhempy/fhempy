d7e3241b9bbd9f91fbc0c24d8e4b36324c152dc9
dominikkarall <dominik.karall@gmail.com>
2023-01-02 15:33:57 +0100

    feat(tuya): support colour_data reading

--- FHEM/bindings/python/fhempy/lib/tuya/tuya.py
+++ FHEM/bindings/python/fhempy/lib/tuya/tuya.py
@@ -621,13 +621,18 @@ class tuya(generic.FhemModule):
                             f"{st['dp_id']} with value {status[dp]}"
                         )
                         if st["type"] == "Json":
-                            flat_json = self.convert_json(status[dp], st)
-                            for name in flat_json:
-                                await fhem.readingsBulkUpdateIfChanged(
-                                    self.hash,
-                                    reading + "_" + name,
-                                    flat_json[name],
+                            if st["code"] in ["colour_data", "colour_data_v2"]:
+                                await self.update_readings_colour(
+                                    st["code"], status[dp]
                                 )
+                            else:
+                                flat_json = self.convert_json(status[dp], st)
+                                for name in flat_json:
+                                    await fhem.readingsBulkUpdateIfChanged(
+                                        self.hash,
+                                        reading + "_" + name,
+                                        flat_json[name],
+                                    )
                         else:
                             if reading == "state":
                                 state_set = True
@@ -778,4 +783,18 @@ class tuya(generic.FhemModule):
             "state",
             f"{count_found} devices found localy",
             1,
+        )
+
+    async def update_readings_colour(self, code, hexcolour):
+        if code == "colour_data" and self.info_dict["category"] == "dj":
+            # only category dj (light) has old colour_data
+            (red, green, blue) = self.tt.BulbDevice._hexvalue_to_rgb(hexcolour, "A")
+        else:
+            (red, green, blue) = self.tt.BulbDevice._hexvalue_to_rgb(hexcolour, "B")
+
+        rgb_hex = f"{red:02x}{green:02x}{blue:02x}"
+        await fhem.readingsBulkUpdate(
+            self.hash,
+            code,
+            rgb_hex,
         )
