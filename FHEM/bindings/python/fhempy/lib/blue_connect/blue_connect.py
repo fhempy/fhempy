0a6d93dbc0f965b92cfb275f9a64bc6fdd8ff2e5
dominikkarall <dominik.karall@gmail.com>
2023-01-10 18:32:55 +0100

    fix(blue_connect): testing use of bleak instead of bluepy

--- FHEM/bindings/python/fhempy/lib/blue_connect/blue_connect.py
+++ FHEM/bindings/python/fhempy/lib/blue_connect/blue_connect.py
@@ -1,25 +1,21 @@
 import asyncio
 import codecs
-import functools
-import time
 
-from .. import fhem, generic, utils
-from ..core import ble
+from bleak import BleakClient, BleakScanner
 
-DEFAULT_TIMEOUT = 1
+from .. import fhem, generic
 
 
 class blue_connect(generic.FhemModule):
     def __init__(self, logger):
         super().__init__(logger)
         self._ble_lock = asyncio.Lock()
-        self._conn = None
         self.water_temp = "0"
         self.water_orp = "0"
         self.water_ph = "0"
         self.water_salt = "0"
         self.water_conductivity = "0"
-        self.other_18 = ""
+        self.rssi = ""
         set_conf = {
             "measure": {"help": "Send signal to start measuring"},
         }
@@ -33,23 +29,16 @@ class blue_connect(generic.FhemModule):
             return "Usage: define my_blueconnect fhempy blue_connect MAC"
         self._mac = args[3]
         self.hash["MAC"] = self._mac
-        self._conn = ble.BTLEConnection(
-            self._mac,
-            keep_connected=True,
-        )
-        self._conn.set_callback("all", self.received_notification)
-        self.create_async_task(self.update_loop())
-        self.create_async_task(self.keep_connected())
+        self.task_update_loop = self.create_async_task(self.update_loop())
 
     async def Undefine(self, hash):
-        if self._conn:
-            self._conn.set_keep_connected(False)
+        self.task_update_loop.cancel()
         return await super().Undefine(self.hash)
 
     async def set_measure(self, hash, params):
         self.create_async_task(self.measure_once())
 
-    def received_notification(self, data):
+    def received_notification(self, uuid, data):
         self.raw_measurement = codecs.encode(data, "hex")
         raw_temp = int(self.raw_measurement[4:6] + self.raw_measurement[2:4], 16)
         self.water_temp = float(raw_temp) / 100
@@ -67,34 +56,41 @@ class blue_connect(generic.FhemModule):
             self.raw_measurement[20:22] + self.raw_measurement[18:20], 16
         )
         self.water_conductivity = round(float(raw_conductivity) / 0.4134)
+        self.rssi = self.device.rssi
 
-    def blocking_measure(self):
-        for cnt in range(0, 10):
-            try:
-                # enable notifications
-                self._conn.write_characteristic(0x0014, b"\x01\x00")
-                # start measuring
-                self._conn.write_characteristic(0x0012, b"\x01", 60)
-                break
-            except BrokenPipeError:
-                self.logger.error("BLE error, reconnect BLE")
-                self._conn.disconnect()
-                self._conn.stop_helper()
-            except Exception:
-                self.logger.exception("Failed to write characteristics")
-                time.sleep(10)
+        self.create_async_task(self.update_readings())
 
-    def blocking_read_others(self):
-        for cnt in range(0, 10):
+    def handle_disconnect(self, _: BleakClient):
+        self.logger.error("Device disconnected")
+
+    async def measure(self):
+        self.device = None
+        for cnt in range(0, 20):
             try:
-                data = self._conn.read_characteristic(0x18)
-                self.other_18 = codecs.encode(data, "hex")
-            except BrokenPipeError:
-                self.logger.error("BLE error, reconnect BLE")
-                self._conn.stop_helper()
+                # find device
+                self.device = await BleakScanner.find_device_by_address(
+                    self._mac, timeout=30
+                )
+                if not self.device:
+                    self.logger.error("Couldn't find device")
+
+                # connect to device
+                async with BleakClient(
+                    self.device, disconnected_callback=self.handle_disconnect
+                ) as client:
+                    # register notify
+                    await client.start_notify(
+                        "F3300003-F0A2-9B06-0C59-1BC4763B5C00",
+                        self.received_notification,
+                    )
+                    # start measure
+                    await client.write_gatt_char(
+                        "F3300002-F0A2-9B06-0C59-1BC4763B5C00", b"\x01"
+                    )
+                    break
             except Exception:
-                self.logger.exception("Failed to read characteristics")
-                time.sleep(10)
+                self.logger.exception("Failed to measure")
+                await asyncio.sleep(10)
 
     async def update_loop(self):
         while True:
@@ -106,29 +102,19 @@ class blue_connect(generic.FhemModule):
                 self.logger.exception("Failed to update readings")
             await asyncio.sleep(7200)
 
-    async def keep_connected(self):
-        while True:
-            try:
-                async with self._ble_lock:
-                    await utils.run_blocking(
-                        functools.partial(self.blocking_read_others)
-                    )
-            except asyncio.CancelledError:
-                break
-            except Exception:
-                self.logger.exception("Failed to update readings")
-            await asyncio.sleep(60)
-
     async def measure_once(self):
-        self.water_temp = 0
-        self.water_orp = 0
-        self.water_ph = 0
-        self.water_conductivity = 0
-        self.water_salt = 0
-
         async with self._ble_lock:
-            await utils.run_blocking(functools.partial(self.blocking_measure))
+            self.water_temp = 0
+            self.water_orp = 0
+            self.water_ph = 0
+            self.water_conductivity = 0
+            self.water_salt = 0
+
+            await self.measure()
+
+    async def update_readings(self):
         await fhem.readingsBeginUpdate(self.hash)
+        await fhem.readingsBulkUpdate(self.hash, "rssi", self.rssi)
         await fhem.readingsBulkUpdate(self.hash, "temperature", self.water_temp)
         await fhem.readingsBulkUpdate(self.hash, "ph", self.water_ph)
         await fhem.readingsBulkUpdate(self.hash, "orp", self.water_orp)
