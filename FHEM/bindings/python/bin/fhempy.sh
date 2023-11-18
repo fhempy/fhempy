0b9d387c4bca526c58c33e527bf148078c582697
fhempy <dominik.karall@gmail.com>
2023-11-19 00:23:48 +0100

    fix(fusionsolar): try to fix local time issue (#219)

---
+++ FHEM/bindings/python/bin/fhempy.sh
@@ -0,0 +1,12 @@
+
+# check if I run in a venv
+
+# if not, check if "venv" exists
+
+# if not, create venv
+
+# activate venv
+source venv/bin/activate
+
+# run fhempy and push all args to it
+fhempy $args
